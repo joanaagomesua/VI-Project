ee2ef771df8524e9ec1891b12b751ab9ab8c2b34
ftferreira02 <tomasfreitas4054@gmail.com>
2024-11-16 18:42:44 +0000

    loaded general stats data

--- cinestatsapp/src/App.js
+++ cinestatsapp/src/App.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import AboutUs from './components/AboutUs/AboutUs';
+import GeneralStats from "./components/GeneralStats/GeneralStats"; // Import GeneralStats component
+import Papa from "papaparse";
 
 function App() {
 
   const [theme, setTheme] = useState('light');
+  const [spectatorsData, setSpectatorsData] = useState([]);
+  const [venuesData, setVenuesData] = useState([]);
+  const [sessionsData, setSessionsData] = useState([]);
 
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
@@ -13,11 +18,43 @@ function App() {
     document.documentElement.setAttribute('data-theme', newTheme);
   };
 
+  // Function to load CSV data
+  const loadCSVData = (filePath, setData) => {
+    Papa.parse(filePath, {
+      download: true,
+      header: true,  // Use the first row as column headers
+      dynamicTyping: true,  // Automatically convert to numbers where possible
+      complete: (result) => {
+        setData(result.data);
+      },
+      error: (error) => {
+        console.error("Error loading CSV file:", error);
+      },
+    });
+  };
+
+  // Use useEffect to load data once when the component mounts
+  useEffect(() => {
+    loadCSVData("/VI_Data/INE 1950-2023 Espetadores(anual).csv", setSpectatorsData);
+    loadCSVData("/VI_Data/INE 1950-2023 Recintos de Cinema(anual).csv", setVenuesData);
+    loadCSVData("/VI_Data/INE 1950-2023 Sessões de Cinema(anual).csv", setSessionsData);
+  }, []);
+
   return (
     <div className="App">
       <Navbar  theme={theme} toggleTheme={toggleTheme} />
       <Routes>
         <Route path="/aboutus" element={<AboutUs />} />
+        <Route
+          path="/generalstats"
+          element={
+            <GeneralStats
+              spectatorsData={spectatorsData}
+              venuesData={venuesData}
+              sessionsData={sessionsData}
+            />
+          }
+        />
       </Routes>
     </div>
   );
